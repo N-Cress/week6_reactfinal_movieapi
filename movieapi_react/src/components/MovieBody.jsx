e113d68f3eaf0991b77a3c98eb8a8ac706191bef
Noah Cress <56100756+N-Cress@users.noreply.github.com>
2024-07-17 03:29:34 +0000

    adjustments to sizing

--- movieapi_react/src/components/MovieBody.jsx
+++ movieapi_react/src/components/MovieBody.jsx
@@ -1,8 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
+
 
 
 const apiRoute = "https://www.omdbapi.com/?apikey=7d7109ef&i=";
